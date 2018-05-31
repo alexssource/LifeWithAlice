3cd3667c92c2445f09ca386b57e012dc5c0def82
Anton.Bykov <Anton.Bykov@life.com.by>
2018-05-31 16:07:42 +0300

    add tariff map

--- src/main/java/by/com/life/alice/domain/TariffDescription.java
+++ src/main/java/by/com/life/alice/domain/TariffDescription.java
@@ -17,4 +17,21 @@ public class TariffDescription {
     private String price;
     private String priceUnit;
     private String link;
+    private String code;
+
+    public TariffDescription(String plan, String description, String internetTraffic, String internetTrafficUnit, String minutes, String minutesUnit, String price, String priceUnit, String link) {
+        this.plan = plan;
+        this.description = description;
+        this.internetTraffic = internetTraffic;
+        this.internetTrafficUnit = internetTrafficUnit;
+        this.minutes = minutes;
+        this.minutesUnit = minutesUnit;
+        this.price = price;
+        this.priceUnit = priceUnit;
+        this.link = link;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
